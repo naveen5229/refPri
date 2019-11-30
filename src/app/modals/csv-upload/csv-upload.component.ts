541f36ee185edf7a7c54af5c38758d62decfebb4
Lalit Singh <singhlalit843@gmail.com>
2019-11-30 12:52:37 +0530

    csv change

--- src/app/modals/csv-upload/csv-upload.component.ts
+++ src/app/modals/csv-upload/csv-upload.component.ts
@@ -13,16 +13,19 @@ export class CsvUploadComponent implements OnInit {
   title = "";
   button = "upload";
   upload = {
-    csv: null
+    csv: null,
+    campaignId: null
   }
   csv: any;
+  campaignDataList = [];
   constructor(public common: CommonService,
     public api: ApiService,
     public activeModal: NgbActiveModal,
     public modalSService: NgbModal) {
     this.title = this.common.params.title;
     this.button = this.common.params.button;
-    this.common.handleModalSize('class', 'modal-lg', '400', 'px');
+    this.common.handleModalSize('class', 'modal-lg', '450', 'px');
+    this.getcampaignList();
 
   }
 
@@ -30,6 +33,19 @@ export class CsvUploadComponent implements OnInit {
   }
   closeModal() {
     this.activeModal.close({ response: false });
+  }
+
+  getcampaignList() {
+    this.common.loading++;
+    this.api.get("CampaignSuggestion/getCampaignList").subscribe(res => {
+      this.common.loading--;
+      this.campaignDataList = res['data'];
+    },
+      err => {
+        this.common.loading--;
+        this.common.showError();
+        console.log('Error: ', err);
+      });
   }
 
 
@@ -47,18 +63,39 @@ export class CsvUploadComponent implements OnInit {
         }
 
         res = res.toString().replace('vnd.ms-excel', 'csv');
-        this.csv = res;
+        this.upload.csv = res;
       }, err => {
         this.common.loading--;
         console.error('Base Err: ', err);
       })
   }
 
-  sampleCsv() {
-    window.open("http://13.126.215.102/sample/csv/sample_document_upload.csv");
-  }
+  // sampleCsv() {
+  //   window.open("http://13.126.215.102/sample/csv/sample_document_upload.csv");
+  // }
 
   uploadCsv() {
+    const params = {
+      CmpTarCsv: this.upload.csv,
+      campaignId: this.upload.campaignId
+    };
+    if (!params.CmpTarCsv && !params.campaignId) {
+      return this.common.showError("Select  Option First");
+    }
+    this.common.loading++;
+    this.api.post('Campaigns/ImportCampTargetCsv', params)
+      .subscribe(res => {
+        this.common.loading--;
+        this.common.showToast(res["msg"]);
 
+        // if (errorData.length) {
+        //   this.common.params = { errorData, ErrorReportComponent, title: 'Document Verification' };
+        //   const activeModal = this.modalService.open(ErrorReportComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
+        // }
+        this.activeModal.close({ response: true });
+      }, err => {
+        this.common.loading--;
+        console.log(err);
+      });
   }
 }
