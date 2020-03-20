5ca02fe23f9d76468f79a6e2cd9c4c552b782006
Akshar Arora <akshar@Akshars-MacBook-Pro.local>
2020-03-20 15:11:49 +0530

    add css to attendance and shift page

--- src/app/Service/Api/api.service.ts
+++ src/app/Service/Api/api.service.ts
@@ -7,8 +7,8 @@ import { UserService } from '../user/user.service';
   providedIn: 'root'
 })
 export class ApiService {
-  // URL: string = 'http://localhost/itrm_webservices/';//komal local
-  URL: string = 'http://192.168.1.109/itrm_webservices/';//komal local
+  URL: string = 'http://localhost/itrm_webservices/';//komal local
+  // URL: string = 'http://192.168.1.109/itrm_webservices/';//komal local
   // URL: string = 'https://dev.elogist.in/itrm_webservices/'
 
   URLBooster: string = 'http://dev.elogist.in/booster_webservices/';
