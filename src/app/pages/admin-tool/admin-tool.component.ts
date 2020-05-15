847033b84ffebfa2aaf39757542db6dd9c0cb04c
Sunil Kumar <sunil.kumar@elogist.in>
2020-05-15 19:27:45 +0530

    add employee detail modal in admin tool

--- src/app/pages/admin-tool/admin-tool.component.ts
+++ src/app/pages/admin-tool/admin-tool.component.ts
@@ -26,111 +26,129 @@ export class AdminToolComponent implements OnInit {
   constructor(public common: CommonService,
     public api: ApiService,
     public modalService: NgbModal,
-    ) { 
-      this.getActiveAdminList();
-    }
+  ) {
+    this.getActiveAdminList();
+  }
 
   ngOnInit() {
   }
 
-  adminTools(){
+  adminTools() {
     const activeModal = this.modalService.open(SaveadminComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
-     }
+  }
 
-    getActiveAdminList() {
-      this.common.loading++;
-      this.api.get('Admin/getAllAdmin')
-        .subscribe(res => {
-          this.common.loading--;
-          // console.log('res:', res);
-          this.activeAdminUserList = res['data'] || [];
-          console.log(this.activeAdminUserList);
-  
-          this.activeAdminUserList.length ? this.setTable() : this.resetTable();
-  
-  
-        }, err => {
-          this.common.loading--;
-          console.log(err);
-        });
-    } 
-    
-    resetTable() {
-      this.table.data = {
-        headings: {},
-        columns: []
-      };
-    }
-    generateHeadings() {
-      // console.log(this.dailyReportList);
-      let headings = {};
-      for (var key in this.activeAdminUserList[0]) {
-        // console.log(key.charAt(0));
-  
-        if (key.charAt(0) != "_") {
-          headings[key] = { title: key, placeholder: this.formatTitle(key) };
-        }
-      }
-      return headings;
-    }
-  
-    formatTitle(strval) {
-      let pos = strval.indexOf('_');
-      if (pos > 0) {
-        return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
-      } else {
-        return strval.charAt(0).toUpperCase() + strval.substr(1);
-      }
-    }
-  
-    setTable() {
-      this.table.data = {
-        headings: this.generateHeadings(),
-        columns: this.getTableColumns()
-      };
-      return true;
-    }
-  
-    getTableColumns() {
-      console.log(this.generateHeadings());
-      let columns = [];
-      this.activeAdminUserList.map(activeAdmin => {
-        let column = {};
-        for (let key in this.generateHeadings()) {
-          if (key == 'Action' || key == 'action') {
-            column[key] = {
-              value: "",
-              isHTML: true,
-              action: null,
-              icons: this.actionIcons(activeAdmin)
-            };
-          } else {
-            column[key] = { value: activeAdmin[key], class: 'black', action: '' };
-          }
-        }
-        columns.push(column);
+  getActiveAdminList() {
+    this.common.loading++;
+    this.api.get('Admin/getAllAdmin')
+      .subscribe(res => {
+        this.common.loading--;
+        // console.log('res:', res);
+        this.activeAdminUserList = res['data'] || [];
+        console.log(this.activeAdminUserList);
+
+        this.activeAdminUserList.length ? this.setTable() : this.resetTable();
+
+
+      }, err => {
+        this.common.loading--;
+        console.log(err);
       });
-      return columns;
-  
-    }
+  }
 
-    actionIcons(activeAdmin) {
-      let icons = [
-        { class: "fa fa-edit", action: this.editActiveAdmin.bind(this, activeAdmin) },
-        // { class: "fa fa-trash", action: this.deleteInstaller.bind(this, installer) },
-      ];
-      return icons;
-    }
+  resetTable() {
+    this.table.data = {
+      headings: {},
+      columns: []
+    };
+  }
+  generateHeadings() {
+    // console.log(this.dailyReportList);
+    let headings = {};
+    for (var key in this.activeAdminUserList[0]) {
+      // console.log(key.charAt(0));
 
-    editActiveAdmin(activeAdmin) {
-      this.common.params = { activeAdminDetail: activeAdmin, title: "Edit Admin", button: "Update" };
-      const activeModal = this.modalService.open(SaveadminComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
-      activeModal.result.then(data => {
-        console.log(data);
-        if (data) {
-          this.getActiveAdminList();
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: key, placeholder: this.formatTitle(key) };
+      }
+    }
+    return headings;
+  }
+
+  formatTitle(strval) {
+    let pos = strval.indexOf('_');
+    if (pos > 0) {
+      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+    } else {
+      return strval.charAt(0).toUpperCase() + strval.substr(1);
+    }
+  }
+
+  setTable() {
+    this.table.data = {
+      headings: this.generateHeadings(),
+      columns: this.getTableColumns()
+    };
+    return true;
+  }
+
+  getTableColumns() {
+    console.log(this.generateHeadings());
+    let columns = [];
+    this.activeAdminUserList.map(activeAdmin => {
+      let column = {};
+      for (let key in this.generateHeadings()) {
+        if (key == 'Action' || key == 'action') {
+          column[key] = {
+            value: "",
+            isHTML: true,
+            action: null,
+            icons: this.actionIcons(activeAdmin)
+          };
+        } else {
+          column[key] = { value: activeAdmin[key], class: 'black', action: '' };
         }
-      })
-    }
-  
+      }
+      columns.push(column);
+    });
+    return columns;
+
+  }
+
+  actionIcons(activeAdmin) {
+    let icons = [
+      { class: "fa fa-edit", action: this.editActiveAdmin.bind(this, activeAdmin) },
+      { class: "fa fa-info-circle", action: this.editAdminInfo.bind(this, activeAdmin) },
+      // { class: "fa fa-trash", action: this.deleteInstaller.bind(this, installer) },
+    ];
+    return icons;
+  }
+
+  editActiveAdmin(activeAdmin) {
+    this.common.params = { activeAdminDetail: activeAdmin, title: "Edit Admin", button: "Update" };
+    const activeModal = this.modalService.open(SaveadminComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
+    activeModal.result.then(data => {
+      console.log(data);
+      if (data) {
+        this.getActiveAdminList();
+      }
+    })
+  }
+
+  closeAdminDetailModal() {
+    document.getElementById("adminDetailModal").style.display = "none";
+  }
+
+  adminDetail = {
+    id: null,
+    name: "",
+    fatherName: ""
+  }
+
+  editAdminInfo(activeAdmin) {
+    console.log("editAdminInfo:", activeAdmin);
+    document.getElementById("adminDetailModal").style.display = "block";
+    this.adminDetail.id = activeAdmin.id;
+    this.adminDetail.name = activeAdmin.name;
+  }
+
 }
