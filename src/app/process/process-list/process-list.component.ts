910d9ce659b398de8319628fac580b38d27ec83a
Sunil kumar <sunil.kumar@elogist.in>
2020-07-14 11:34:45 +0530

    add process modal

--- src/app/process/process-list/process-list.component.ts
+++ src/app/process/process-list/process-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../Service/Api/api.service';
+import { CommonService } from '../../Service/common/common.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddProcessComponent } from '../../modals/process-modals/add-process/add-process.component';
 
 @Component({
   selector: 'ngx-process-list',
@@ -6,10 +10,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./process-list.component.scss']
 })
 export class ProcessListComponent implements OnInit {
+  processData = [];
+  table = {
+    data: {
+      headings: {},
+      columns: [],
+    },
+    settings: {
+      hideHeader: true
+    }
+  };
 
-  constructor() { }
+  constructor(public api: ApiService, public common: CommonService, public modalService: NgbModal) {
+    this.getProcessList();
+  }
 
   ngOnInit() {
+  }
+
+  getProcessList() {
+    this.common.loading++;
+    this.api.get('Processes/getProcessList')
+      .subscribe(res => {
+        this.common.loading--;
+        console.log("api data", res);
+        if (!res['data']) return;
+        this.processData = res['data'];
+        this.processData.length ? this.setTable() : this.resetTable();
+
+      }, err => {
+        this.common.loading--;
+        console.log(err);
+      });
+  }
+
+  addProcess() {
+    this.common.params = { title: "Add Campaign", button: "Add" }
+    const activeModal = this.modalService.open(AddProcessComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
+    activeModal.result.then(data => {
+      if (data.response) {
+        this.getProcessList();
+      }
+    });
+  }
+
+  resetTable() {
+    this.table.data = {
+      headings: {},
+      columns: []
+    };
+  }
+
+  setTable() {
+    this.table.data = {
+      headings: this.generateHeadings(),
+      columns: this.getTableColumns()
+    };
+    return true;
+  }
+
+  generateHeadings() {
+    let headings = {};
+    for (var key in this.processData[0]) {
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: key, placeholder: this.common.formatTitle(key) };
+      }
+    }
+    return headings;
+  }
+
+  getTableColumns() {
+    let columns = [];
+    this.processData.map(process => {
+      let column = {};
+      for (let key in this.generateHeadings()) {
+        if (key == 'Action') {
+          column[key] = {
+            value: "",
+            isHTML: false,
+            action: null,
+            icons: this.actionIcons(process)
+          };
+        } else {
+          column[key] = { value: process[key], class: 'black', action: '' };
+        }
+      }
+      columns.push(column);
+    })
+
+    return columns;
+  }
+
+  actionIcons(process) {
+    let icons = [
+      { class: "far fa-edit", action: null },
+    ];
+    return icons;
   }
 
 }
