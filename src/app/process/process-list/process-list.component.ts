cb38f7977f07ac2fce774c903f9bab380dee3f04
Sunil kumar <sunil.kumar@elogist.in>
2020-07-15 18:36:42 +0530

    update process-list page

--- src/app/process/process-list/process-list.component.ts
+++ src/app/process/process-list/process-list.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../Service/Api/api.service';
 import { CommonService } from '../../Service/common/common.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddProcessComponent } from '../../modals/process-modals/add-process/add-process.component';
+import { DataMappingComponent } from '../../modals/campaign-modals/data-mapping/data-mapping.component';
 
 @Component({
   selector: 'ngx-process-list',
@@ -44,8 +45,8 @@ export class ProcessListComponent implements OnInit {
       });
   }
 
-  addProcess() {
-    this.common.params = { title: "Add Campaign", button: "Add" }
+  addProcess(processDate = null) {
+    this.common.params = { title: "Add Process", button: "Next", editData: processDate };
     const activeModal = this.modalService.open(AddProcessComponent, { size: 'md', container: 'nb-layout', backdrop: 'static' });
     activeModal.result.then(data => {
       if (data.response) {
@@ -84,7 +85,7 @@ export class ProcessListComponent implements OnInit {
     this.processData.map(process => {
       let column = {};
       for (let key in this.generateHeadings()) {
-        if (key == 'Action') {
+        if (key.toLowerCase() == 'action') {
           column[key] = {
             value: "",
             isHTML: false,
@@ -103,9 +104,101 @@ export class ProcessListComponent implements OnInit {
 
   actionIcons(process) {
     let icons = [
-      { class: "far fa-edit", action: null },
+      { class: "far fa-edit", title: "Edit", action: this.addProcess.bind(this, process) },
+      { class: "fas fa-list-alt pri_cat ml-2", action: this.openCatModal.bind(this, process, 1), title: "Primary Category Mapping" },
+      { class: "fas fa-list-alt ml-2", action: this.openCatModal.bind(this, process, 2), title: "Secondary Category Mapping" },
     ];
     return icons;
+  }
+
+  catType = 1;
+  catList = [];
+  tableCatList = {
+    data: {
+      headings: {},
+      columns: [],
+    },
+    settings: {
+      hideHeader: true
+    }
+  };
+
+  closeCatModal() {
+    document.getElementById("catModal").style.display = "none";
+  }
+
+  openCatModal(process, type) {
+    this.catType = type;
+    let apiName = "Processes/getProcessPriCat?processId=" + process._id;
+    if (type == 1) {
+      apiName = "Processes/getProcessPriCat?processId=" + process._id;
+    } else {
+      apiName = "Processes/getProcessSecCat?processId=" + process._id;
+    }
+    this.common.loading++;
+    this.api.get(apiName).subscribe(res => {
+      this.common.loading--;
+      console.log("api data", res);
+      if (!res['data']) return;
+      this.processData = res['data'];
+      this.catList.length ? this.setTableCatList() : this.resetTableCatList();
+
+    }, err => {
+      this.common.loading--;
+      console.log(err);
+    });
+    document.getElementById("catModal").style.display = "block";
+  }
+
+  resetTableCatList() {
+    this.tableCatList.data = {
+      headings: [],
+      columns: []
+    };
+  }
+
+  setTableCatList() {
+    this.tableCatList.data = {
+      headings: this.generateHeadingsCatList(),
+      columns: this.getTableColumnsCatList()
+    };
+    return true;
+  }
+
+  generateHeadingsCatList() {
+    let headings = {};
+    for (var key in this.catList[0]) {
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: key, placeholder: this.common.formatTitle(key) };
+      }
+    }
+    return headings;
+  }
+
+  getTableColumnsCatList() {
+    let columns = [];
+    this.catList.map(cat => {
+      let column = {};
+      for (let key in this.generateHeadingsCatList()) {
+        if (key.toLowerCase() == 'action') {
+          column[key] = {
+            value: "",
+            isHTML: false,
+            action: null,
+            icons: this.actionIconsCatList(cat)
+          };
+        } else {
+          column[key] = { value: cat[key], class: 'black', action: '' };
+        }
+      }
+      columns.push(column);
+    })
+
+    return columns;
+  }
+
+  actionIconsCatList(cat) {
+    console.log("cat:", cat);
   }
 
 }
