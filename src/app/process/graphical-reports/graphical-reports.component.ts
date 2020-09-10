c6fcaa77751c303a31624496afa20d5b745f7d4e
Naveen Gupta <naveen1.axestrack@gmail.com>
2020-09-10 18:10:18 +0530

    changes in graphical-reports updated

--- src/app/process/graphical-reports/graphical-reports.component.ts
+++ src/app/process/graphical-reports/graphical-reports.component.ts
@@ -9,60 +9,75 @@ import { ApiService } from '../../Service/Api/api.service';
   styleUrls: ['./graphical-reports.component.scss']
 })
 export class GraphicalReportsComponent implements OnInit {
+  processList = [];
   assign = {
     data: [],
     filter: [],
     chart: []
   }
+  processId = '';
 sideBarData = [
-  {  title : 'Action-1',
-    hideMe: true,
-    children:[
-      { title : 'subtab1'},
-      { title : 'subtab2'},
-      { title : 'subtab3'},
-      { title : 'subtab4'}
-    ]
+  {title:'Transaction Fields', children:[{title:'',children:''}]
   },
-    { title: 'Action-2',
-    hideMe: true,
-      children:[
-        { title : 'subtab5'},
-        { title : 'subtab6'},
-        { title : 'subtab7jjjjjjjjjjjjjjjjjj'},
-        { title : 'subtab8'}
-      ]
-    },{ title: 'Action-3',
-    hideMe: true,
-    children:[
-      { title : 'subtab9'},
-      { title : 'subtab10'},
-      { title : 'subtab11'},
-      { title : 'subtab12'}
-    ]
-  }
-]
+  {title:'Form Fields', children:[{title:'',children:''}]
+  },
+];
 
   constructor(
     public common: CommonService,
     public api: ApiService,) {
-      this.getSideBarData();
+      this.getProcessList();
      }
 
   ngOnInit() {
   }
 
-  getSideBarData(){
+  getProcessList() {
     this.common.loading++;
-    this.api.get('Processes/getAllReportFieldsForNav?processId=2').subscribe(res => {
+    this.api.get('Processes/getProcessList').subscribe(res => {
       this.common.loading--;
       if (!res['data']) return;
-      console.log('Data:',res['data']);
+      this.processList = res['data'];
 
     }, err => {
       this.common.loading--;
       console.log(err);
     });
+  }
+
+  getSideBarData(processId){
+    this.processId = processId;
+    if(!this.processId){
+      this.common.showError('Select Process')
+    }else{
+    this.common.loading++;
+    this.api.get(`Processes/getAllReportFieldsForNav?processId=${this.processId['_id']}`).subscribe(res => {
+      this.common.loading--;
+      if (!res['data']) return;
+      this.resetSidebarData();
+      let sideBarData = res['data'];
+      sideBarData.map(ele=> {
+        this.sideBarData.map(data=>{
+          if(data.title == ele.reftype){
+            data.children.push({title:ele.title,children:JSON.parse(ele.children)})
+          }
+        })
+      })
+      console.log('Data:',this.sideBarData);
+
+    }, err => {
+      this.common.loading--;
+      console.log(err);
+    });}
+  }
+
+  resetSidebarData(){
+    this.sideBarData = [
+      {title:'Transaction Fields', children:[{title:'',children:''}]
+      },
+      {title:'Form Fields', children:[{title:'',children:''}]
+      },
+    ];
   }
 
 
@@ -75,7 +90,18 @@ sideBarData = [
       // moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       console.log("if2:", event.previousContainer.data);
-      this.assign.data.push(event.previousContainer.data[event.previousIndex]);
+      
+      let exists =0;
+      this.assign.data.forEach(ele=> {
+        if(ele.r_colid === event.previousContainer.data[event.previousIndex]['r_colid'] &&
+        (ele.r_isdynamic === event.previousContainer.data[event.previousIndex]['r_isdynamic'] &&
+        ele.r_ismasterfield === event.previousContainer.data[event.previousIndex]['r_ismasterfield'])){
+            exists++;
+        };
+      })
+      if(exists > 0) return; this.assign.data.push(event.previousContainer.data[event.previousIndex]);
+      
+      
       console.log('stored:',this.assign.data)
       // transferArrayItem(event.previousContainer.data,event.container.data,event.previousIndex,event.currentIndex);
     }
@@ -94,5 +120,21 @@ sideBarData = [
   removeField(index){
     this.assign.data.splice(index,1)
     console.log('deleted:',index,'from:',this.assign.data)
+  }
+
+  getReportPreview(){
+      let params = {
+      processId:this.processId['_id'],
+      reportFilter:null,
+      info:JSON.stringify(this.assign.data)
+    };
+      this.common.loading++;
+      this.api.post('Processes/getPreviewGraphicalReport',params).subscribe(res=>{
+          this.common.loading--;
+          console.log('Response:',res);
+      },err=>{
+        this.common.loading--;
+        console.log('Error:',err)
+      })
   }
 }
