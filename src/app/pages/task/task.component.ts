715f87a918367e54b43c2ff29903d6fc1a06c1a9
Sunil kumar <sunil.kumar@elogist.in>
2020-09-18 16:56:25 +0530

    auto ack task by assignee when first comment send

--- src/app/pages/task/task.component.ts
+++ src/app/pages/task/task.component.ts
@@ -1425,7 +1425,7 @@ export class TaskComponent implements OnInit {
     }
 
     if (type == -101) {
-      if ([101, 102, 104].includes(ticket._tktype)) {
+      if ([101, 102, 104, 111, 112, 113].includes(ticket._tktype)) {
         icons.push({
           class: "fas fa-trash-alt",
           action: this.deleteTicket.bind(this, ticket, type),
@@ -1493,7 +1493,7 @@ export class TaskComponent implements OnInit {
             title: "Mark Task as Hold",
           });
         }
-        if (ticket._tktype == 104) {//leave reject
+        if ([104, 111, 112, 113].includes(ticket._tktype)) {//leave reject
           icons.push({
             class: "fa fa-times text-danger",
             action: this.changeTicketStatusWithConfirm.bind(this, ticket, type, -1),
@@ -1545,7 +1545,7 @@ export class TaskComponent implements OnInit {
           title: "Mark Rejected",
         });
       } else if (
-        ([101, 102, 104].includes(ticket._tktype)) &&
+        ([101, 102, 104, 111, 112, 113].includes(ticket._tktype)) &&
         ticket._cc_user_id &&
         !ticket._cc_status
       ) {
@@ -2516,7 +2516,7 @@ export class TaskComponent implements OnInit {
       } else if (subTabType == 4) {
         //leave
         selectedList = this.normalTaskListAll.filter((x) => {
-          return x._tktype == 104;
+          return (x._tktype == 104 || x._tktype == 111 || x._tktype == 112 || x._tktype == 113);
         });
       } else {
         //all
@@ -2544,7 +2544,7 @@ export class TaskComponent implements OnInit {
       } else if (subTabType == 4) {
         //leave
         selectedList = this.normalTaskByMeListAll.filter((x) => {
-          return x._tktype == 104;
+          return (x._tktype == 104 || x._tktype == 111 || x._tktype == 112 || x._tktype == 113);
         });
       } else {
         //all
@@ -2558,7 +2558,7 @@ export class TaskComponent implements OnInit {
       if (subTabType == 4) {
         //leave
         selectedList = this.ccTaskListAll.filter((x) => {
-          return x._tktype == 104;
+          return (x._tktype == 104 || x._tktype == 111 || x._tktype == 112 || x._tktype == 113);
         });
       } else {
         //all
