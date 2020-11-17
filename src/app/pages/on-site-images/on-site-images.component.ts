be64565aa27b8c82a9fd8a0d3b0ee4939df86550
Sunil kumar <sunil.kumar@elogist.in>
2020-11-17 18:22:38 +0530

    update process-chatbox and onsiteimages page

--- src/app/pages/on-site-images/on-site-images.component.ts
+++ src/app/pages/on-site-images/on-site-images.component.ts
@@ -25,12 +25,22 @@ export class OnSiteImagesComponent implements OnInit {
     }
   };
 
+  transActionList = null;
+  tableTransActionList = {
+    data: {
+      headings: {},
+      columns: []
+    },
+    settings: {
+      hideHeader: true
+    }
+  };
+
   constructor(public common: CommonService, public user: UserService, public api: ApiService, public modalService: NgbModal) {
     this.getAdminReportsByUser()
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   getAdminReportsByUser() {
     this.table = {
@@ -46,30 +56,23 @@ export class OnSiteImagesComponent implements OnInit {
     let startDate = this.common.dateFormatter(this.startDate);
     let endDate = this.common.dateFormatter(this.endDate);
     const params = `?startDate=${startDate}&endDate=${endDate}`;
-    console.log(params);
     // return;
     this.common.loading++;
-    this.api.get('Admin/getOnSiteImagesByUser' + params, 'I')
-      .subscribe(res => {
-        this.common.loading--;
-        console.log('res:', res);
-        if (res['code'] > 0) {
-          if (res['data']) {
-            this.adminReportList = res['data'] || [];
-            this.adminReportList.length ? this.setTable() : this.setTable();
-            console.log(this.adminReportList);
-          }
-        } else {
-          this.common.showError(res['msg']);
+    this.api.get('Admin/getOnSiteImagesByUser' + params, 'I').subscribe(res => {
+      this.common.loading--;
+      if (res['code'] > 0) {
+        if (res['data']) {
+          this.adminReportList = res['data'] || [];
+          this.adminReportList.length ? this.setTable() : this.setTable();
+          console.log(this.adminReportList);
         }
-
-
-      }, err => {
-        this.common.loading--;
-        console.log(err);
-      });
-
-
+      } else {
+        this.common.showError(res['msg']);
+      }
+    }, err => {
+      this.common.loading--;
+      console.log(err);
+    });
   }
 
   setTable() {
@@ -86,19 +89,10 @@ export class OnSiteImagesComponent implements OnInit {
       console.log(key.charAt(0));
 
       if (key.charAt(0) != "_") {
-        headings[key] = { title: key, placeholder: this.formatTitle(key) };
+        headings[key] = { title: key, placeholder: this.common.formatTitle(key) };
       }
     }
     return headings;
-  }
-
-  formatTitle(strval) {
-    let pos = strval.indexOf('_');
-    if (pos > 0) {
-      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
-    } else {
-      return strval.charAt(0).toUpperCase() + strval.substr(1);
-    }
   }
 
   getTableColumns() {
@@ -131,20 +125,126 @@ export class OnSiteImagesComponent implements OnInit {
   }
 
   actionIcons(adminReport) {
-    let icons = [];
-    icons.push({ class: !adminReport._refid ? "fa fa-paperclip gray" : "fa fa-paperclip", action: this.openDataModal.bind(this, adminReport), txt: "", title: 'report', });
+    let icons = [
+      { class: !adminReport._refid ? "fa fa-paperclip gray" : "fa fa-paperclip", action: this.openDataModal.bind(this, adminReport), txt: "", title: 'report', },
+      { class: "fas fa-sitemap", action: this.openTransActionListModal.bind(this, adminReport), txt: "", title: 'Txn Action', }
+    ];
+    // icons.push({ class: !adminReport._refid ? "fa fa-paperclip gray" : "fa fa-paperclip", action: this.openDataModal.bind(this, adminReport), txt: "", title: 'report', });
     return icons;
   }
   openDataModal(adminReport) {
     console.log("OnSiteImagesComponent -> openReportModal -> adminReport", adminReport);
-    if(adminReport._action_name && adminReport._identity && adminReport._process){
-      
+    if (adminReport._action_name && adminReport._identity && adminReport._process) {
+
     }
     this.common.showToast('working');
   }
 
-  closeDataModal(){
+  closeDataModal() {
     document.getElementById('dataWindow').style.display = 'none';
+  }
+
+  openTransActionListModal(adminReport) {
+    this.getProcessLeadByType();
+    document.getElementById('transActionList').style.display = 'block';
+  }
+
+  closeTransActionListModal() {
+    document.getElementById('transActionList').style.display = 'none';
+  }
+
+  getProcessLeadByType() {
+    let type = 1, startDate = null, endDate = null;
+    this.common.loading++;
+    let params = "?type=" + type + "&startDate=" + startDate + "&endDate=" + endDate;
+    this.api.get("Processes/getMyProcessByType" + params).subscribe(res => {
+      this.common.loading--;
+      if (res['code'] == 1) {
+        this.transActionList = res['data'] || [];
+        this.setTableTransActionList();
+      } else {
+        this.common.showError(res['msg']);
+      }
+    }, err => {
+      this.common.loading--;
+      this.common.showError();
+      console.log('Error: ', err);
+    });
+  }
+
+  // start: TransActionList
+  setTableTransActionList() {
+    this.tableTransActionList.data = {
+      headings: this.generateHeadingsTransActionList(),
+      columns: this.getTableColumnsTransActionList()
+    };
+    return true;
+  }
+
+  generateHeadingsTransActionList() {
+    let headings = {};
+    for (var key in this.transActionList[0]) {
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: key, placeholder: this.common.formatTitle(key) };
+      }
+      if (key === "addtime" || key === "action_completed") {
+        headings[key]["type"] = "date";
+      }
+    }
+    return headings;
+  }
+
+  getTableColumnsTransActionList() {
+    let columns = [];
+    this.transActionList.map(lead => {
+      let column = {};
+      for (let key in this.generateHeadingsTransActionList()) {
+        if (key.toLowerCase() == 'action') {
+          column[key] = {
+            value: "",
+            isHTML: true,
+            action: null,
+            icons: this.actionIconsForTransAction(lead)
+          };
+        } else if (key == 'action_expdate' && new Date(lead[key]) < this.common.getDate()) {
+          column[key] = { value: lead[key], class: 'black font-weight-bold', action: '' };
+        } else {
+          column[key] = { value: lead[key], class: 'black', action: '' };
+        }
+        column['style'] = { 'background': this.common.taskStatusBg(lead._status) };
+      }
+      columns.push(column);
+    });
+    return columns;
+  }
+  // end: TransActionList
+
+  actionIconsForTransAction(lead) {
+    let icons = [
+      { class: "fa fa-plus", action: this.mapOnSiteImageWithTransAction.bind(this, lead), txt: "", title: 'Map with on-site-image', },
+    ];
+    return icons;
+  }
+
+  mapOnSiteImageWithTransAction(lead) {
+    let transActionId = null, onSiteImageId = null;
+    this.common.loading++;
+    let params = {
+      transActionId: transActionId,
+      onSiteImageId: onSiteImageId
+    }
+    this.api.post("Processes/mapOnSiteImageWithTransAction", params).subscribe(res => {
+      this.common.loading--;
+      if (res['code'] == 1) {
+        this.common.showToast(res['msg']);
+      } else {
+        this.common.showError(res['msg']);
+      }
+    }, err => {
+      this.common.loading--;
+      this.common.showError();
+      console.log('Error: ', err);
+    });
   }
 
 }
