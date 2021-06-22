fb87193f7d4cb0e4e49392169ea94dc4290dda49
Manish <manish.jangid@axestrack.com>
2021-06-22 15:55:32 +0530

    added category dropdown

--- src/app/pages/expense-type/expense-type.component.ts
+++ src/app/pages/expense-type/expense-type.component.ts
@@ -1,6 +1,7 @@
 import { group } from 'console';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'ngx-expense-type',
@@ -10,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
 export class ExpenseTypeComponent implements OnInit {
   expenseType:any;
   status:any;
+  categories:any[] = [];
+
+
 
   constructor() {
 
   }
+
+
+getCategoris(){
+let catrgories = from ([[
+{name:'Car'},
+{name:'Bus'},
+{name:'Hotel'},
+{name:'Bike'},
+{name:'Truck'},
+{name:'Travel'},
+]])
+
+catrgories.subscribe((item:any)=>{
+this.categories = item;
+console.log('this.categories: ', this.categories);
+
+})
+
+
+}
+
+selectedCategory(event:any){
+
+
+}
+
+
 
 SubmitExpenses(){
 let params = {
@@ -21,11 +52,15 @@ expoenseType:this.expenseType,
 status:this.status
 }
 
-
 }
 
 
+
+
+
   ngOnInit() {
+  this.getCategoris();
+
   }
 
 }
