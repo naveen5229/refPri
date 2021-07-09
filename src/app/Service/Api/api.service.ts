e9b05cf241205e341bd157ad483cb9b4b0cec614
sunil kumar <sunil.kumar@elogist.in>
2021-07-09 14:20:09 +0530

    update dev url

--- src/app/Service/Api/api.service.ts
+++ src/app/Service/Api/api.service.ts
@@ -8,10 +8,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-   I_URL: string = 'http://localhost/itrm_webservices/';
+  //  I_URL: string = 'http://localhost/itrm_webservices/';
   // I_URL: string = 'http://192.168.0.101/itrm_webservices/';
 
-  //  I_URL: string = 'https://dev.elogist.in/itrm_webservices/';
+   I_URL: string = 'https://dev.elogist.in/itrm_webservices/';
   B_URL: string = 'https://dev.elogist.in/booster_webservices/';
   T_URL: string = 'https://dev.elogist.in/transtrucknew/';
 
